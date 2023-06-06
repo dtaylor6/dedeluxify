f2c73abac31445cb5233d5781ff9f45ff5051b06
Daniel <dtaylor6@umbc.edu>
2023-06-05 22:06:33 -0400

    Debounce spotify search

--- src/components/hooks/useSpotifySearch.js
+++ src/components/hooks/useSpotifySearch.js
@@ -2,16 +2,27 @@ import { useEffect, useState } from 'react'
 
 import { SearchAlbums } from '../../services/SpotifyPlaybackService'
 
+// How many ms to wait to search after user stops typing
+const SEARCH_TIMEOUT = 1000
+
 // Fetch album query from Spotify service
 const useSpotifySearch = (query) => {
   const [results, setResults] = useState(undefined)
 
+  // Wait for user to finish typing before sending search request
   useEffect(() => {
     const getQuery = async () => {
       const queryResult = await SearchAlbums(query)
       setResults(queryResult)
     }
-    getQuery()
+
+    let debouncer = setTimeout(() => {
+      getQuery()
+    }, SEARCH_TIMEOUT)
+
+    return () => {
+      clearTimeout(debouncer)
+    }
   }, [query])
 
   return results
