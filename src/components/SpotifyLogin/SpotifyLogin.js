72529c0aa7138c9fdd5d0fb47a139d1e36c7bb2b
Daniel <dtaylor6@umbc.edu>
2023-07-13 00:32:04 -0400

    Add login buttons

--- src/components/SpotifyLogin/SpotifyLogin.js
+++ src/components/SpotifyLogin/SpotifyLogin.js
@@ -10,9 +10,14 @@ const SpotifyLogin = () => {
 
   return (
     <LoginWrapper>
+      <h1>Log in</h1>
       <LoginButton onClick={() => window.location.href=url}>
         <SpotifyLogo />
         <LoginSpan>Log in with Spotify</LoginSpan>
+      </LoginButton>
+      <br/>
+      <LoginButton onClick={() => console.log('Preview mode')}>
+        <LoginSpan>Preview without logging in</LoginSpan>
       </LoginButton>
     </LoginWrapper>
   );
--- src/components/SpotifyLogin/SpotifyLogin.style.js
+++ src/components/SpotifyLogin/SpotifyLogin.style.js
@@ -13,11 +13,25 @@ const LoginWrapper = styled.div`
 `;
 
 const LoginSpan = styled.span`
-  margin-left: 0.5rem;
+  margin-left: 1rem;
 `;
 
 const LoginButton = styled.button`
+  white-space: pre-line;
+  margin: 1rem 1rem;
+  font-size: 2rem;
+  height: 4rem;
+  width: 30rem;
+  text-align: center;
+  border: 0.2rem solid black;
+  border-radius: 0.25rem;
 
+  &:hover {
+    transition: background-color 0.5s ease;
+    background-color: black;
+    border-radius: 0.25rem;
+    color: #F3F3F3;
+  }
 `;
 
 export {
