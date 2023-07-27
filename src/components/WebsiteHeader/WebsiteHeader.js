bf2bc46ac859f9771137983d59949dbb912adf24
Daniel <dtaylor6@umbc.edu>
2023-07-27 01:54:53 -0400

    Add ability to close profile menu with click

--- src/components/WebsiteHeader/WebsiteHeader.js
+++ src/components/WebsiteHeader/WebsiteHeader.js
@@ -13,6 +13,7 @@ const DEFAULT_PFP = 'https://i.scdn.co/image/ab6761610000517458efbed422ab4648446
 const WebsiteHeader = (props) => {
   const profilePicSrc = props.profilePic ? props.profilePic : DEFAULT_PFP;
   const profileRef = useRef(null);
+  const btnRef = useRef(null);
 
   // Hide search results when clicking outside of search bar or search results box
   const [showWindow, setShowWindow] = useState(false);
@@ -21,21 +22,33 @@ const WebsiteHeader = (props) => {
       if (!e.target) {
         setShowWindow(false);
       }
-      else if (profileRef.current && profileRef.current.contains(e.target)){
-        setShowWindow(true);
-      }
-      else {
-        // Clicked outside the box
+      else if (
+        btnRef.current
+        && profileRef.current
+        && !btnRef.current.contains(e.target)
+        && !profileRef.current.contains(e.target)
+      ) {
         setShowWindow(false);
       }
     });
   }, []);
 
+  const toggleWindow = (event) => {
+    event.preventDefault();
+    setShowWindow(!showWindow);
+  };
+
   return(
     <HeaderWrapper>
       <WebsiteLogo marginTop="1rem" marginBottom="2rem" marginLeft="1rem" />
       <ImgWrapper ref={profileRef}>
-        <StyledInput type="image" src={profilePicSrc} showWindow={showWindow} />
+        <StyledInput
+          type="image"
+          src={profilePicSrc}
+          showWindow={showWindow}
+          onClick={toggleWindow}
+          ref={btnRef}
+        />
         {showWindow && <ProfileWindow />}
       </ImgWrapper>
     </HeaderWrapper>
