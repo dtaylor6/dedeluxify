a80d1aa2265e46513369e30cbf7bd8f864fbc773
Daniel <dtaylor6@umbc.edu>
2023-08-10 22:42:35 -0400

    Create preferences cypress file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'zy18h2',
   e2e: {
     baseUrl: 'http://localhost:3000',
     chromeWebSecurity: false,
---
+++ cypress/e2e/preferences.cy.js
@@ -0,0 +1,27 @@
+describe('querying', () => {
+  before(() => {
+    cy.visit('/login');
+    cy.get('#spotify-login-button').click();
+
+    // Redirect to Spotify login page
+    cy.origin('https://accounts.spotify.com', () => {
+      const email = Cypress.env('spotify_email');
+      const password = Cypress.env('spotify_password');
+      cy.get('input#login-username').type(email, { log: false });
+      cy.get('input#login-password').type(password, { log: false });
+      cy.get('button#login-button').click();
+    });
+
+    cy.url().should('contain', 'localhost');
+    cy.get('#spotify-player');
+  });
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('At home page', () => {
+    cy.url().should('contain', 'localhost');
+    cy.get('#spotify-player');
+  });
+});
