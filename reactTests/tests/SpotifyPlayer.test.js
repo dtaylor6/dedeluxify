e03483bab98baa3d0a6792123b48b9aad1bd9a28
Daniel <dtaylor6@umbc.edu>
2023-08-16 13:58:52 -0400

    Add more SpotifyPlayer button tests

--- reactTests/tests/SpotifyPlayer.test.js
+++ reactTests/tests/SpotifyPlayer.test.js
@@ -15,7 +15,7 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }));
 
-describe('track tests', () => {
+describe('button tests', () => {
   beforeEach(() => {
     jest.resetAllMocks();
     mockedGetPreview = jest
@@ -33,5 +33,77 @@ describe('track tests', () => {
     const pauseBtn = screen.getByTitle('Pause');
     await user.click(pauseBtn);
     expect(pauseBtn.title).toBe('Play');
+  });
+
+  test('changes back to pause on two clicks', async () => {
+    render(<SpotifyPlayer />);
+    const user = userEvent.setup();
+    const pauseBtn = screen.getByTitle('Pause');
+    await user.click(pauseBtn);
+    expect(pauseBtn.title).toBe('Play');
+    await user.click(pauseBtn);
+    expect(pauseBtn.title).toBe('Pause');
+  });
+
+  test('skip to next track', async () => {
+    render(<SpotifyPlayer />);
+    const user = userEvent.setup();
+    const nextBtn = screen.getByTitle('Next');
+    expect(screen.getByText('Track 1'));
+    await user.click(nextBtn);
+    expect(screen.getByText('Track 2'));
+  });
+
+  test('go to previous track', async () => {
+    render(<SpotifyPlayer />);
+    const user = userEvent.setup();
+    const nextBtn = screen.getByTitle('Next');
+    expect(screen.getByText('Track 1'));
+    await user.click(nextBtn);
+    expect(screen.getByText('Track 2'));
+
+    const prevBtn = screen.getByTitle('Previous');
+    expect(screen.getByText('Track 2'));
+    await user.click(prevBtn);
+    expect(screen.getByText('Track 1'));
+  });
+
+  test('mute button changes on click', async () => {
+    render(<SpotifyPlayer />);
+    const user = userEvent.setup();
+    const muteBtn = screen.getByTitle('Mute');
+    await user.click(muteBtn);
+    expect(muteBtn.title).toBe('Unmute');
+  });
+
+  test('volume slider changes on mute', async () => {
+    render(<SpotifyPlayer />);
+    const user = userEvent.setup();
+    const muteBtn = screen.getByTitle('Mute');
+    const slider = screen.getByTitle('Volume');
+    expect(slider.value).toBe('50');
+
+    // Mute volume
+    await user.click(muteBtn);
+    expect(muteBtn.title).toBe('Unmute');
+    expect(slider.value).toBe('0');
+  });
+
+  test('volume slider changes back on unmute', async () => {
+    render(<SpotifyPlayer />);
+    const user = userEvent.setup();
+    const muteBtn = screen.getByTitle('Mute');
+    const slider = screen.getByTitle('Volume');
+    expect(slider.value).toBe('50');
+
+    // Mute
+    await user.click(muteBtn);
+    expect(muteBtn.title).toBe('Unmute');
+    expect(slider.value).toBe('0');
+
+    // Unmute
+    await user.click(muteBtn);
+    expect(muteBtn.title).toBe('Mute');
+    expect(slider.value).toBe('50');
   });
 });
